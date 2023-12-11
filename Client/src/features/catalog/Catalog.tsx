2c7da643d8426b50002af57320a83e8be7f76265
jakub-zulewski <j.zulewski@outlook.com>
2023-12-11 13:18:12 +0100

    Add Axios and product page.

--- Client/src/features/catalog/Catalog.tsx
+++ Client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
+import axios from "axios";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    axios
+      .get("http://localhost:5000/api/products")
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) return <h3>Loading...</h3>;
 
   return <ProductList products={products} />;
 }
