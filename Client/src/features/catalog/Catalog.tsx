c9414d36f11ba46c44a7628bfa617ac0cc294b71
jakub-zulewski <j.zulewski@outlook.com>
2023-12-12 17:58:44 +0100

    Add Axios agent to centralize requests.

--- Client/src/features/catalog/Catalog.tsx
+++ Client/src/features/catalog/Catalog.tsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
-import axios from "axios";
+import agent from "../../app/api/agent";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/products")
-      .then((response) => setProducts(response.data))
+    agent.Catalog.list()
+      .then((products) => setProducts(products))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, []);
