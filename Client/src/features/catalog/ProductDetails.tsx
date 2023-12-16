591cbb38068c8dfb805fc1bef1a8f76f5fc91134
jakub-zulewski <j.zulewski@outlook.com>
2023-12-16 13:30:08 +0100

    Add update item quantity in basket on product details page.

--- Client/src/features/catalog/ProductDetails.tsx
+++ Client/src/features/catalog/ProductDetails.tsx
@@ -6,9 +6,10 @@ import {
   TableCell,
   TableContainer,
   TableRow,
+  TextField,
   Typography,
 } from "@mui/material";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
@@ -17,13 +18,21 @@ import { router } from "../../app/router/Routes";
 import { toast } from "react-toastify";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { currencyFormat } from "../../app/util/util";
+import { useStoreContext } from "../../app/context/storeContextState";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
+  const { basket, setBasket, removeItem } = useStoreContext();
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(0);
+  const [submitting, setSubmitting] = useState(false);
+  const item = basket?.items.find((i) => i.productId === product?.id);
 
   useEffect(() => {
+    if (item) setQuantity(item.quantity);
+
     id &&
       agent.Catalog.details(parseInt(id))
         .then((response) => setProduct(response))
@@ -32,7 +41,35 @@ export default function ProductDetails() {
           router.navigate("/");
         })
         .finally(() => setLoading(false));
-  }, [id]);
+  }, [id, item]);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    if (parseInt(event.currentTarget.value) >= 0) {
+      setQuantity(parseInt(event.currentTarget.value));
+    }
+  }
+
+  function handleUpdateCart() {
+    if (!product) return;
+
+    setSubmitting(true);
+
+    if (!item || quantity > item.quantity) {
+      const updatedQuantity = item ? quantity - item.quantity : quantity;
+
+      agent.Basket.addItem(product.id!, updatedQuantity)
+        .then((basket) => setBasket(basket))
+        .catch(() => toast.error("Something went wrong."))
+        .finally(() => setSubmitting(false));
+    } else {
+      const updatedQuantity = item.quantity - quantity;
+
+      agent.Basket.removeItem(product.id!, updatedQuantity)
+        .then(() => removeItem(product.id!, updatedQuantity))
+        .catch(() => toast.error("Something went wrong."))
+        .finally(() => setSubmitting(false));
+    }
+  }
 
   if (loading) return <LoadingComponent message="Loading product..." />;
 
@@ -79,6 +116,34 @@ export default function ProductDetails() {
             </TableBody>
           </Table>
         </TableContainer>
+        <Grid mt={2} container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              onChange={handleInputChange}
+              variant="outlined"
+              type="number"
+              label="Quantity in cart"
+              fullWidth
+              value={quantity}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <LoadingButton
+              disabled={
+                item?.quantity === quantity || (!item && quantity === 0)
+              }
+              loading={submitting}
+              onClick={handleUpdateCart}
+              sx={{ height: "55px" }}
+              color="primary"
+              size="large"
+              variant="contained"
+              fullWidth
+            >
+              {item ? "Update quantity" : "Add to cart"}
+            </LoadingButton>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
