97c5e3f6079d500ae51634245ad91e1e193d807f
jakub-zulewski <j.zulewski@outlook.com>
2023-12-21 17:07:37 +0100

    Add sorting to product list endpoint.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,8 +12,15 @@ public class ProductsController(StoreContext storeContext) : BaseApiController
     private readonly StoreContext _storeContext = storeContext;
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
-        => await _storeContext.Products.ToListAsync();
+    public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+    {
+        var query = _storeContext.Products
+            .AsNoTracking()
+            .Sort(orderBy)
+            .AsQueryable();
+
+        return await query.ToListAsync();
+    }
 
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
---
+++ API/Extensions/ProductExtensions.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+
+namespace API.Extensions;
+
+public static class ProductExtensions
+{
+    public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
+    {
+        if (string.IsNullOrWhiteSpace(orderBy))
+            return query.OrderBy(x => x.Name);
+
+        query = orderBy switch
+        {
+            "price" => query.OrderBy(x => x.Price),
+            "priceDesc" => query.OrderByDescending(x => x.Price),
+            _ => query.OrderBy(x => x.Name)
+        };
+
+        return query;
+    }
+}
