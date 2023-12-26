52682c22c70926d42de442a4790b76739d3d28c0
jakub-zulewski <j.zulewski@outlook.com>
2023-12-26 22:21:28 +0100

    Add Swagger.

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,9 +37,58 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddScoped<TokenService>();
 
+builder.Services.AddSwaggerGen(configuration =>
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Place your JWT token here with prefix \"Bearer \".",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    configuration.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    configuration.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            jwtSecurityScheme, Array.Empty<string>()
+        }
+    });
+
+    configuration.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "React Shop API",
+        Description = "API for React Shop"
+    });
+});
+
 builder.Services.AddCors();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(configuration
+        => configuration.RouteTemplate = "docs/swagger/{documentname}/swagger.json");
+
+    app.UseSwaggerUI(configuration =>
+    {
+        configuration.SwaggerEndpoint("/docs/swagger/v1/swagger.json", "React Shop API");
+
+        configuration.RoutePrefix = "docs";
+
+        configuration.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
+}
 
 app.UseMiddleware<ExceptionMiddleware>();
 
